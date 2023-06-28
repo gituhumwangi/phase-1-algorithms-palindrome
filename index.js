7a33c3ffad8873ac063240a5b6e55ed73afe5439
dennis <dennis.mwangi@student.moringaschool.com>
2023-06-28 22:53:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  //find the length of the string
+  const len = word.length
+
+  for(let i = 0; i < len/2 ; i++){
+
+   return (word[i] !== word[len - 1 -i] ? false : true)
+  }
 }
 
 /* 
   Add your pseudocode here
 */
-
+//The program takes strings as input
+//the inputs should be all in lowercases
+//the argument takes in one string and should return a true or a false depending on the functionality of the code
+//the program should have condition to return whether true or false
+//inputs:string  output:boolean
 /*
   Add written explanation of your solution here
 */
